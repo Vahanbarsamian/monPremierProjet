ea42f7854c879593373b1f73d4855e6e6e4bd037
Vahan  Barsamian <vahanbarsamian@gmail.com>
2018-11-23 08:13:26 +0100

    Mise a jour search.php

--- controller/search.php
+++ controller/search.php
@@ -14,6 +14,7 @@ $poids = htmlspecialchars($param['poids']);
 $delai = htmlspecialchars($param['delai']);
 
 // Valeur de l'id et du dossier des fichiers
+$messagerie = true;
 $iduser=ceil($_SESSION['iduser']);
 $dossier = "../fichiers/Datacarrier/user".$iduser;
 
@@ -23,15 +24,29 @@ if (!empty($dep) && !empty($colis) && !empty($poids)){
 		while(false !== ($fichier = readdir($listdirectory))){
 
 		}
-		$fichier = array_diff(scandir($dossier), array('.','..'));
-		$filename = $_SERVER['DOCUMENT_ROOT'].'/fichiers/Datacarrier/user'.$_SESSION['iduser'].'/listeDesTransporteurs.csv';
-		var_dump($fichier,count($fichier),$filename);
-		$open = new FileOpenClass($filename); 
-		$mesFichiers = 
-		// Vérification des données
-		
 	}
+	$fichier = array_diff(scandir($dossier), array('.','..'));
+	$filename = $_SERVER['DOCUMENT_ROOT'].'/fichiers/userLog'.$_SESSION['iduser'].'/listeDesTransporteurs.csv';
+	$open = new FileOpenClass($filename); 
+	$mesFichiers = $open->fileToArray($filename);
 
+		// Vérification des données et choix des transporteurs en fonctions des données
+	if (($poids / $colis)>30 || $type == 1){
+		$messagerie = false;
+	} 
+	foreach($mesFichiers as $value){
+		foreach($fichier as $name){
+			$tableauDeValeurs = [];
+			if(in_array($name,$value)){
+				if(($messagerie == false) && ($value[2] == "Messagerie")){
+					continue;
+				}else{
+					var_dump($value,$messagerie,$value[2]);	
+					
+				}
+			}
+		}
+	}
 }
 echo json_encode(['result'=>true]);
 
